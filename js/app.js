34ff74472b68c864b03f35cb7f85e3301831b038
David Zapata <dzg00@iesemilidarder.com>
2021-02-28 13:46:11 +0100

    d

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const username = document.getElementById("username")
+const enterButon = document.getElementById("enterButton")
+init();
+
+function init(){
+
+}
+
+
+enterButon.addEventListener("click", () => {
+    
+})
